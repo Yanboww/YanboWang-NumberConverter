45d4704d6649072d4377ddad79bf1a35bef6f676
unknown <BT_4N2_24@BKTECH.LOCAL>
2024-01-08 13:47:39 -0500

    Initial commit

---
+++ src/ConverterRunner.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class ConverterRunner {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Number Converter!");
+        System.out.println("--------------------------------");
+        System.out.print("Enter the base of your number (2, 8 or 10): ");
+
+        Scanner s = new Scanner(System.in);
+        String choice = s.nextLine();
+        if(!choice.equals("2") && !choice.equals("8") && !choice.equals("10"))
+        {
+            System.out.print("Please enter a valid base (2, 8 or 10): ");
+            choice = s.nextLine();
+        }
+        int base = Integer.parseInt(choice);
+
+        System.out.print("Enter your number: ");
+        String number = s.nextLine();
+        int n = Integer.parseInt(number);
+
+        s.close();
+
+        NumberConverter nc = new NumberConverter(n, base);
+        int[] digits = nc.getDigits();
+        System.out.println("\n\nDigit array: " + Arrays.toString(digits));
+        System.out.println("Number: " + nc.displayOriginalNumber());
+        System.out.println(nc.convertArray(nc.convertToBinary(),2));
+        System.out.println(nc.convertArray(nc.convertToOctal(),8));
+    }
+}
+
---
+++ src/NumberConverter.java
@@ -0,0 +1,111 @@
+import java.util.HashMap;
+public class NumberConverter {
+    int[] digits;
+    int base;
+    private HashMap digitDict;
+
+    public NumberConverter(int number, int base) {
+        String numberAsString = Integer.toString(number);
+        digits = new int[numberAsString.length()];
+        for (int i = 0; i < numberAsString.length(); i++) {
+            String single = numberAsString.substring(i,i+1);
+            int d = Integer.parseInt(single);
+            digits[i] = d;
+        }
+        this.base = base;
+    }
+
+    public String displayOriginalNumber() {
+        String o = "";
+        for (int i = 0; i < digits.length; i++) {
+            o = o + digits[i];
+        }
+        o = o + "\n";
+        return o;
+    }
+
+    public int[] getDigits() {
+        return digits;
+    }
+
+    public int[] convertToDecimal()
+    {
+        int[] decimalValue = new int[digits.length];
+        int count = 0;
+        for(int i = digits.length-1;i>=0;i--)
+        {
+            int currentValue = digits[i];
+            decimalValue[i] = currentValue * (int)Math.pow(base,count);
+            count++;
+        }
+        return decimalValue;
+    }
+
+    public int[] convertToBinary() {
+        int[] binaryValue = new int[digits.length*4];
+        int decimal = convertArrayToDecimal(convertToDecimal());
+        for(int i = binaryValue.length-1;i>=0;i--)
+        {
+            if(decimal%2!=0)
+            {
+                binaryValue[i] = 1;
+            }
+            else
+            {
+                binaryValue[i]=0;
+            }
+            decimal /=2;
+        }
+        return binaryValue;
+    }
+
+    public int[] convertToOctal()
+    {
+        int decimal = convertArrayToDecimal(convertToDecimal());
+        int[] octalValue = new int[digits.length*4];
+        for(int i = octalValue.length-1;i>=0;i--)
+        {
+            octalValue[i] = decimal%8;
+            decimal/=8;
+        }
+        return octalValue;
+    }
+
+    public int convertArrayToDecimal(int[] array)
+    {
+        int total = 0;
+        for(int value : array)
+        {
+            total+=value;
+        }
+        return total;
+    }
+
+    public String convertArray(int[] array, int base)
+    {
+        if(base == 10)
+        {
+            return Integer.toString(convertArrayToDecimal(array));
+        }
+        else{
+            String returnValue = "";
+            boolean start = false;
+            for(int value : array)
+            {
+                if(start){
+                    returnValue+= Integer.toString(value);
+                }
+                else{
+                    if(value!=0)
+                    {
+                        start = true;
+                        returnValue+=Integer.toString(value);
+                    }
+
+                }
+            }
+            return returnValue;
+        }
+    }
+}
+
